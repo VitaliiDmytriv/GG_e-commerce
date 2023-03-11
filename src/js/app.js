5bc1b83e71ac514ede7bf7a948026963cad195b4
Vitalii Dmytriv <vitaliqq.dmutriv@gmail.com>
2023-03-11 16:04:10 +0200

    done row section

--- src/js/app.js
+++ src/js/app.js
@@ -64,4 +64,45 @@ $(document).ready(function () {
         const targetPosition = target.offset().top;
         $("html, body").animate({ scrollTop: targetPosition }, 600);
     });
+
+    // slider =============================================
+
+    const fontSize = parseInt($("html").css("font-size"));
+    const width800px = fontSize * 50;
+    const width1300px = fontSize * 81.25;
+
+    new Swiper(".row__slider", {
+        breakpoints: {
+            // when window width is >= 320px
+            200: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            // // when window width is >= 480px
+            [width800px]: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+            },
+            // // when window width is >= 640px
+            [width1300px]: {
+                slidesPerView: 4,
+                spaceBetween: 40,
+            },
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            type: "fraction",
+
+            renderFraction: function (currentClass, totalClass) {
+                return `<div class="row__curentSlide curentSlide ${currentClass}"></div>
+                        <div class="row__AllSlides AllSlides ${totalClass}"></div>
+                `;
+            },
+        },
+
+        navigation: {
+            nextEl: ".goRigth",
+            prevEl: ".goLeft",
+        },
+    });
 });
